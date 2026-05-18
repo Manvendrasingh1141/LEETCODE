24e7843d90388c52f93461523649278f985b90f2
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-05-18 22:50:45 +0530

    Time: 2 ms (37.02%) | Memory: 10.7 MB (7.69%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        string temp = "",check="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='A' && s[i]<='Z')temp+=(s[i]+32);
+            else if(s[i]>='a' && s[i]<='z')temp+=s[i];
+            else if(s[i]>='0' && s[i]<='9')temp+=s[i];
+        }
+        check=temp;
+        reverse(temp.begin(),temp.end());
+        if(temp==check)return 1;
+        return 0;
+    }
+};
+
+
