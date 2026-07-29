3904196f34d1ab130d48179784326a2a270e85d9
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-07-29 08:02:13 +0530

    Time: 0 ms (100%), Space: 28.3 MB (63.09%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0)nums[i]=0;
+            else nums[i]=1;
+        }
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+        return nums;
+    }
+};
