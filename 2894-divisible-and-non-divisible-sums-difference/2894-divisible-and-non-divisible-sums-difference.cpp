d9c6317b6a45fc1f21805ee477bb04d18c09c6bb
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-08-03 12:25:33 +0530

    Time: 0 ms (100%), Space: 8.4 MB (66.64%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
@@ -3,18 +3,11 @@ public:
     int s1=0,s2=0;
     void sumOUT(int n,int m,int i){
         if(i>n)return;
-        
-        if(i%m!=0)s2=s2+i;
+        if(i%m==0)s1+=i;
+        if(i%m!=0)s2+=i;
         sumOUT(n,m,i+1);
     }
-    void sumIN(int n,int m,int i){
-        if(i>n)return;
-
-        if(i%m==0)s1=s1+i;
-        sumIN(n,m,i+1);     
-    }
     int differenceOfSums(int n, int m) {
-        sumIN(n,m,1);
         sumOUT(n,m,1);
         return s2-s1;
     }
