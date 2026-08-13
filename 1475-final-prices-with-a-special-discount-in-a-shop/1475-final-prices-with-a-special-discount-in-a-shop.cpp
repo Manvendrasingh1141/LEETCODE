e19bb497f43a9d4b537c59976d5304c4c976ec3e
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-08-13 21:31:45 +0530

    Time: 0 ms (100%), Space: 14 MB (84.64%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& p) {
+        vector<int>ans(p.begin(),p.end());
+        stack<int>st;
+        for(int i=0;i<p.size();i++){
+            while(!st.empty() && p[st.top()]>=p[i]){
+                ans[st.top()]=p[st.top()]-p[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+
+
