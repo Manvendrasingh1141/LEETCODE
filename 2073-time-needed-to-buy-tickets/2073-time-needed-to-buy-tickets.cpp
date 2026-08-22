3cb5d4531631202792b32c322ca4c517548cc752
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-08-22 11:32:12 +0530

    Time: 0 ms (100%), Space: 10.5 MB (46.77%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& nums, int k) {
-        queue<int>q;
         int cnt=0;
-        for(int i=0;i<nums.size();i++)q.push(i);
-
-        while(nums[k]!=0){
-            nums[q.front()]--;
-            if(nums[q.front()]) 
-                q.push(q.front());
-                q.pop();
-                cnt++;
-            
+        for(int i=0;i<nums.size();i++){
+            if(i<=k)cnt+=min(nums[k],nums[i]);
+            else cnt+=min(nums[k]-1,nums[i]);
         }
         return cnt;
     }
