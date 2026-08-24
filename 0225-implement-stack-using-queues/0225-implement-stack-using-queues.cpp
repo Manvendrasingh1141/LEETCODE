fb5bc87075cd9d4bbd0cd17f488a3b70299a3398
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-08-24 10:39:53 +0530

    Time: 0 ms (100%), Space: 10 MB (58.36%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,70 @@
+class MyStack {
+public:
+    queue<int>q1;
+    queue<int>q2;
+    MyStack() {
+    }
+    
+    void push(int x) {
+        if(empty())q1.push(x);
+        else if(q1.empty())q2.push(x);
+        else q1.push(x);
+    }
+    
+    int pop() {
+        if(empty())return 0;
+        else if(q1.empty()){ //elements are in q2
+            while(q2.size()>1){
+                q1.push(q2.front());
+                q2.pop();
+            }
+            int el = q2.front();
+            q2.pop();
+            return el; 
+        }else{//elements are in q1
+            while(q1.size()>1){
+                q2.push(q1.front());
+                q1.pop();
+            }
+            int el = q1.front();
+            q1.pop();
+            return el; 
+        }
+    }
+    
+    int top() {
+        if(empty())return 0;
+        else if(q1.empty()){ //elements are in q2
+            while(q2.size()>1){
+                q1.push(q2.front());
+                q2.pop();
+            }
+            int el = q2.front();
+            q1.push(el);
+            q2.pop();
+            return el; 
+        }else{//elements are in q1
+            while(q1.size()>1){
+                q2.push(q1.front());
+                q1.pop();
+            }
+            int el = q1.front();
+            q2.push(el);
+            q1.pop();
+            return el; 
+        }
+    }
+    
+    bool empty() {
+        return q1.empty() && q2.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
