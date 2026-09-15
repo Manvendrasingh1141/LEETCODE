ad3b047d022f89c6d14cffb86886f2ed95f2bfa5
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-09-15 20:54:13 +0530

    Time: 0 ms (100%), Space: 10.8 MB (89.52%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>&res){
-        if(!root)return;
-        inorder(root->left,res);
-        res.push_back(root->val);
-        inorder(root->right,res);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>res;
-        inorder(root,res);
-        return res;
+        vector<int>ans;
+        if(!root)return ans;
+
+
+        //Morris Traversal
+        while(root){
+            //left part doesn't exsists
+            if(!root->left){
+                ans.push_back(root->val);
+                root = root->right;
+            }
+
+            //left part exists
+            else{
+                //create one current node and go to right;
+                TreeNode *curr = root->left;
+                while(curr->right && curr->right!=root)curr=curr->right;
+
+                //curr->right == NULL
+                if(curr->right == NULL){
+                    //create a link and move to left
+                    curr->right = root;
+                    root = root->left;
+                }
+                //curr->right == root->val
+                else{
+                    //remove the link first , store the value and move to right
+                    curr->right = NULL;
+                    ans.push_back(root->val);
+                    root = root->right;
+                }
+            }
+        }
+
+        return ans;
     }
 };
