ee3c09177ee187f59d711097067e937b10b10921
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-09-16 00:32:55 +0530

    Time: 23 ms (19.33%), Space: 65.8 MB (44.36%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -1,144 +1,29 @@
 class Solution {
 public:
-    class Node {
-    public:
-        int data, height;
-        Node *left, *right;
+    void inorder(TreeNode* root,vector<int>& arr) {
+        if (!root)return;
 
-        Node(int val) {
-            data = val;
-            left = right = NULL;
-            height = 1;
-        }
-    };
-
-    int getHeight(Node* root) {
-        if (!root)
-            return 0;
-
-        return root->height;
+        inorder(root->left, arr);
+        arr.push_back(root->val);
+        inorder(root->right, arr);
     }
 
-    int getBalance(Node* root) {
-        return getHeight(root->left) - getHeight(root->right);
-    }
+    TreeNode* buildBST(vector<int>& arr,int start,int end) {
+        if (start>end)return NULL;
 
-    Node* rightRotation(Node* root) {
-        Node* child = root->left;
-        Node* childRight = child->right;
+        int mid = start+(end-start)/2;
 
-        child->right = root;
-        root->left = childRight;
+        TreeNode* root = new TreeNode(arr[mid]);
 
-        // update heights
-        root->height = 1 + max(getHeight(root->left), getHeight(root->right));
-
-        child->height =
-            1 + max(getHeight(child->left), getHeight(child->right));
-
-        return child;
-    }
-
-    Node* leftRotation(Node* root) {
-        Node* child = root->right;
-        Node* childLeft = child->left;
-
-        child->left = root;
-        root->right = childLeft;
-
-        // update heights
-        root->height = 1 + max(getHeight(root->left), getHeight(root->right));
-
-        child->height =
-            1 + max(getHeight(child->left), getHeight(child->right));
-
-        return child;
-    }
-
-    Node* createAVL(Node* root, int key) {
-
-        if (!root) {
-            return new Node(key);
-        }
-
-        if (root->data > key) {
-            root->left = createAVL(root->left, key);
-        } else if (root->data < key) {
-            root->right = createAVL(root->right, key);
-        } else {
-            return root; // duplicate
-        }
-
-        // update height
-        root->height = 1 + max(getHeight(root->left), getHeight(root->right));
-
-        // calculate balance
-        int balance = getBalance(root);
-
-        // LL
-        if (balance > 1 && key < root->left->data) {
-            return rightRotation(root);
-        }
-
-        // LR
-        else if (balance > 1 && key > root->left->data) {
-            root->left = leftRotation(root->left);
-            return rightRotation(root);
-        }
-
-        // RL
-        else if (balance < -1 && key < root->right->data) {
-            root->right = rightRotation(root->right);
-            return leftRotation(root);
-        }
-
-        // RR
-        else if (balance < -1 && key > root->right->data) {
-            return leftRotation(root);
-        }
+        root->left = buildBST(arr,start,mid-1);
+        root->right = buildBST(arr,mid+1,end);
 
         return root;
     }
 
-    // Store all elements of original BST
-    void storeValues(TreeNode* root, vector<int>& values) {
-        if (!root)
-            return;
-
-        values.push_back(root->val);
-
-        storeValues(root->left, values);
-        storeValues(root->right, values);
-    }
-
-    // Convert AVL Node -> TreeNode
-    TreeNode* convertToTreeNode(Node* root) {
-        if (!root)
-            return NULL;
-
-        TreeNode* newRoot = new TreeNode(root->data);
-
-        newRoot->left = convertToTreeNode(root->left);
-        newRoot->right = convertToTreeNode(root->right);
-
-        return newRoot;
-    }
-
     TreeNode* balanceBST(TreeNode* root) {
-
-        // Step 1: Store all elements
-        vector<int> values;
-
-        storeValues(root, values);
-
-        // Step 2: Create AVL
-        Node* avlRoot = NULL;
-
-        for (int value : values) {
-            avlRoot = createAVL(avlRoot, value);
-        }
-
-        // Step 3: Convert Node tree back to TreeNode
-        return convertToTreeNode(avlRoot);
+        vector<int> arr;
+        inorder(root, arr);
+        return buildBST(arr,0,arr.size()-1);
     }
 };
