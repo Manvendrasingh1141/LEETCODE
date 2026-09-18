613e75ea1852561898232656d34e7fdf82787b17
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-09-18 15:39:27 +0530

    Time: 3 ms (61.3%), Space: 10.1 MB (77.46%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,36 +1,46 @@
 class Solution {
 public:
-    bool check(int n,int row,int col,vector<string>&board){
-        int i=row,j=col;
+    // bool check(int n,int row,int col,vector<string>&board){
+    //     int i=row,j=col;
 
-        while(i>-1 && j>-1){
-            if(board[i][j]=='Q')return 0;
-            i--;
-            j--;
-        }
+    //     while(i>-1 && j>-1){
+    //         if(board[i][j]=='Q')return 0;
+    //         i--;
+    //         j--;
+    //     }
 
-        i=row , j=col;
-        while(i>-1 && j<n){
-            if(board[i][j]=='Q')return 0;
-            i--;
-            j++;
-        }
+    //     i=row , j=col;
+    //     while(i>-1 && j<n){
+    //         if(board[i][j]=='Q')return 0;
+    //         i--;
+    //         j++;
+    //     }
 
-        return 1;
-    }
+    //     return 1;
+    // }
 
-    void fun(int row,int n,vector<bool>&col,vector<vector<string>>&ans,vector<string>&board){
+    void fun(int row,int n,vector<bool>&col, vector<bool>&leftDiagonal, vector<bool>&rightDiagonal,
+    vector<vector<string>>&ans,vector<string>&board){
         if(row==n){
             ans.push_back(board);
             return;
         }
-
+        /*
+        leftDiagonal  : (n-1)+(col-row)
+        rightDiagonal : col+row
+        */
         for(int j=0;j<n;j++){
-            if(col[j]==0 && check(n,row,j,board)){
+            if(!col[j] && !leftDiagonal[n-1+j-row] && !rightDiagonal[row+j]){
                 col[j]=1;
+                leftDiagonal[n-1+j-row]=1;
+                rightDiagonal[row+j]=1;
                 board[row][j] = 'Q';
-                fun(row+1,n,col,ans,board);
+
+                fun(row+1,n,col,leftDiagonal,rightDiagonal,ans,board);
+
                 board[row][j] = '.';
+                leftDiagonal[n-1+j-row]=0;
+                rightDiagonal[row+j]=0;
                 col[j]=0;
             }
         }
@@ -44,7 +54,10 @@ public:
         board[i].push_back('.');
 
         vector<bool>col(n,0);
-        fun(0,n,col,ans,board);
+
+        vector<bool>leftDiagonal(2*n-1,0);
+        vector<bool>rightDiagonal(2*n-1,0);
+        fun(0,n,col,leftDiagonal,rightDiagonal,ans,board);
 
         return ans;
     }
